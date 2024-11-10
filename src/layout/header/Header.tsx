b8235154e9a7d0da03f42a1798feab456d75a441
Anatoliy-Nelson <76780510+Anatoliy-Nelson@users.noreply.github.com>
2024-11-10 22:15:32 +0300

    add heder

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt=""/>
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">#home</a>
+                    </li>
+                    <li>
+                        <a href="">#works</a>
+                    </li>
+                    <li>
+                        <a href="">#about-me</a>
+                    </li>
+                    <li>
+                        <a href="">#contcts</a>
+                    </li>
+                    <li>
+                        <a href="">EN</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
