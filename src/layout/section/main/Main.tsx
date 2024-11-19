cce5acb190bce79ec4f4d3b093c598f2865dbdcf
Anatoliy Manzhola <sirius_star@inbox.ru>
2024-11-19 19:41:04 +0300

    upd icon-sprite

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -2,40 +2,46 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/Cap-and-hidie.png'
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Projects} from "../projects/Projects";
 
 export const Main = () => {
     return (
         <StyledMain>
             <FlexWrapper align={"center"} justify={"space-around"}>
                 <div>
-                    <h1>Elias is a web designer and front-end developer</h1>
-                    <span>He crafts responsive websites where technologies meet creativity</span>
-                    <button>Contact me!!</button>
+                        <h2>Elias is a web designer and front-end developer</h2>
+                        <span>He crafts responsive websites where technologies meet creativity</span>
+                        <button>Contact me!!</button>
                 </div>
                 <div>
                     <Photo src={photo} alt=""/>
                     <span>Currently working on Portfolio</span>
                 </div>
                 <div>
-                    <h2>With great power comes great electricity bill</h2>
+                    <h3>With great power comes great electricity bill</h3>
                     <p>- Dr. Wh</p>
-
-
                 </div>
             </FlexWrapper>
         </StyledMain>
 
-    );
+);
 };
 
 const StyledMain = styled.div`
-    min-height: 100vh;
-    
+min-height: 100vh;
+
 `
 
 const Photo = styled.img`
-    width: 457px;
-    height: 386px;
-    object-fit: cover;
+width: 457px;
+height: 386px;
+object-fit: cover;
+
+`
+const StyledH2 = styled.h2`
+
+`
+
+const StyledH3 = styled.h3`
 
 `
---
+++ src/layout/section/projects/Projects.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Projects = () => {
+    return (
+        <StyledProjects>
+
+        </StyledProjects>
+    );
+};
+
+const StyledProjects = styled.section`
+    min-height: 100vh;
+    background-color: #6d87c8;
+`
+
